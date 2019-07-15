a7dd9e52e12ea55521ec75d6f8ece4c442360674
Xetera <mythologyali@gmail.com>
2019-07-14 22:25:37 -0700

    pulling images from the filesystem

--- src/Waifu/Image.hs
+++ src/Waifu/Image.hs
@@ -1,6 +1,15 @@
 module Waifu.Image where
 
 import Codec.Picture
+import System.Directory
+
+placeholderPath :: FilePath
+placeholderPath = "assets"
 
 getImage :: IO (Either String DynamicImage)
-getImage = readImage "assets/cute.png"
+getImage = readImage "assets/kawaii.jpg"
+
+allPlaceholders :: IO (Either String [DynamicImage])
+allPlaceholders = do
+  dirs <- listDirectory placeholderPath
+  sequence <$> traverse readImage dirs
