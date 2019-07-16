943463d733b58d87a1c0f8ed103af55c818d0707
Xetera <mythologyali@gmail.com>
2019-07-15 22:07:33 -0700

    basic homepage with blaze

--- src/Waifu/Server.hs
+++ src/Waifu/Server.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Waifu.Server
   ( app
   ) where
@@ -8,14 +10,16 @@ import Control.Monad.Reader
 import Network.Wai
 import Servant
 
+import Servant.HTML.Blaze
+import Text.Blaze.Html as H
 import Waifu.Core
 import Waifu.Image
 import Waifu.Servant
+import Waifu.Server.Views.Home
 import Waifu.Util
 
 type ImageAPI
-  =    "image" :> Capture "height" Int :> Capture "width" Int :> Get '[PNG, JPG] Image
-  :<|> "image" :> Get '[PNG, JPG] Image
+   = "image" :> Capture "height" Int :> Capture "width" Int :> Get '[ PNG, JPG] Image :<|> "image" :> Get '[ PNG, JPG] Image :<|> Get '[ HTML] H.Html
 
 app :: [Image] -> Application
 app = serve (Proxy @ImageAPI) . server
@@ -23,15 +27,18 @@ app = serve (Proxy @ImageAPI) . server
 server :: [Image] -> Server ImageAPI
 server images = hoistServer (Proxy @ImageAPI) (runWaifuT images) serverT
 
-serverT :: forall m. (MonadIO m, MonadError ServantErr m) => ServerT ImageAPI (WaifuT m)
-serverT = getRandomSized :<|> getRandom
+serverT ::
+     forall m. (MonadIO m, MonadError ServantErr m)
+  => ServerT ImageAPI (WaifuT m)
+serverT = getRandomSized :<|> getRandom :<|> homePage
   where
+    homePage :: WaifuT m H.Html
+    homePage = return home
     getRandomSized :: Int -> Int -> WaifuT m Image
     getRandomSized h w = do
       images <- ask
       image <- liftIO $ randomList images
       pure $ resize (h, w) image
-
     getRandom :: WaifuT m Image
     getRandom = do
       images <- ask
---
+++ src/Waifu/Server/Views/Home.hs
@@ -0,0 +1,6 @@
+module Waifu.Server.Views.Home where
+
+import Text.Blaze.Html5 as H
+
+home :: H.Html
+home = H.body "yes"
