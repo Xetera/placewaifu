c4b9a2751ef26479fec91a788f6290a115eb1884
1computer1 <mineox100@gmail.com>
2019-07-16 01:20:36 -0400

    Fix width/height order

--- src/Waifu/Server.hs
+++ src/Waifu/Server.hs
@@ -14,7 +14,7 @@ import Waifu.Servant
 import Waifu.Util
 
 type ImageAPI
-  =    "image" :> Capture "height" Int :> Capture "width" Int :> Get '[PNG, JPG] Image
+  =    "image" :> Capture "width" Int :> Capture "height" Int :> Get '[PNG, JPG] Image
   :<|> "image" :> Get '[PNG, JPG] Image
 
 app :: [Image] -> Application
@@ -27,10 +27,10 @@ serverT :: forall m. (MonadIO m, MonadError ServantErr m) => ServerT ImageAPI (W
 serverT = getRandomSized :<|> getRandom
   where
     getRandomSized :: Int -> Int -> WaifuT m Image
-    getRandomSized h w = do
+    getRandomSized w h = do
       images <- ask
       image <- liftIO $ randomList images
-      pure $ resize (h, w) image
+      pure $ resize (w, h) image
 
     getRandom :: WaifuT m Image
     getRandom = do
