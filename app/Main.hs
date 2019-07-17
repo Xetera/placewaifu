e835549f987e423781a96e66d53ad5668182585d
1computer1 <mineox100@gmail.com>
2019-07-16 20:56:10 -0400

    Add CLI Run like `placewaifu -a ./assets`

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
 module Main where
 
+import Options.Applicative
 import Waifu.Server
 
+data Options = Options
+    { assetsFolder :: String
+    }
+
+options :: Parser Options
+options = Options
+    <$> option str (long "assets" <> short 'a' <> help "location of assets folder")
+
+argsParse :: ParserInfo Options
+argsParse = info (helper <*> options)
+    (fullDesc <> progDesc "Run placewaifu" <> header "An incredibly cute placeholder service")
+
 main :: IO ()
-main = runServer "./assets" 1234
+main = do
+    Options { assetsFolder } <- execParser argsParse
+    runServer assetsFolder 1234
