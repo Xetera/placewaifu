4c256f3d0c64adf0e0c781dc3152aeb502343a8b
Xetera <mythologyali@gmail.com>
2019-07-21 12:26:36 -0700

    changed image view to masonry

--- web/js/image_list.js
+++ web/js/image_list.js
@@ -1,26 +1,31 @@
 const $ = document.querySelector.bind(document);
 const dest = $(".images");
-var msnry = new Masonry(dest, {
-  columnWidth: 200
-});
 fetch("/images")
   .then(res => res.json())
   .then(images => {
     const elems = images.sort((x, y) => x.name.localeCompare(y.name)).map(
       image =>
-        `
-        <div class="card">
-          <div class="card-image">
+        `<div class="grid-item">
             <img class="image" src="data:image/${image.format};base64, ${image.data}">
-          </div>
-          <div class="card-content">
-            <p class="float-left">${image.name}</p>
-            <a class="float-right is-6" href="${image.source}">source</a>
-          </div>
-        </div>
-      `
+            <div class="image-info">
+              <div class="image-name">
+                ${image.name}
+              </div>
+              <a class="image-source" href="${image.source}" target="_blank" rel="noopener noreferrer">
+                Source
+              </a>
+            </div>
+        </div>`
     );
     dest.innerHTML = elems.join("\n");
+    imagesLoaded(dest, () => {
+      const msnry = new Isotope(dest, {
+        itemSelector: ".grid-item",
+        masonry: {
+          columnWidth: 300
+        }
+      });
+    })
   })
   .catch(err => {
     dest.innerText = `Something went wrong...`;
